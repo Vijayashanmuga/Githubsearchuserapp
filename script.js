a709bbfb8b2cdae8b08f2b0dee93ef6614083b7c
VIJAYALAKSHMI B <113011990+Vijayashanmuga@users.noreply.github.com>
2024-07-16 09:43:43 +0530

    msg

---
+++ script.js
@@ -0,0 +1,8 @@
+// function git(){
+//     var originalName =document.getElementById("text").value;
+//     console.log(originalName);
+//     fetch("https://api.github.com/users/"+originalName).then((result)=>result.json()).then((data)=>{
+//         console.log(data);
+//       document.getElementById("result").innerHTML;
+//     });
+// }
